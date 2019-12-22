d667c05f0f72d81322bc9d39963e324006d5c43c
Yuzuki Arai <yudukikun5120@gmail.com>
2019-12-22 21:51:51 +0900

    Initialized

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Ichikosai-Auto-Retweeting
 Ichikosai (the school festival of Ibaraki Prefectural Tsuchiura First High School, Japan) twitter account needs to retweet each committees' tweet automatically.
+
+## Notice
+I don't know how to continuously catch new tweets, so I use Heroku Scheduler temporarily.
+
+# Prerequisites
+- Installation of gem "twitter"
+
+```
+gem i twitter
+```
---
+++ retweet.rb
@@ -0,0 +1,22 @@
+require 'twitter'
+
+# Config
+@client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["CONSUMER_KEY"]
+  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+  config.access_token        = ENV["CONSUMER_TOKEN"]
+  config.access_token_secret = ENV["CONSUMER_TOKEN_SECRET"]
+end
+
+# PRODUCTION ENVIRONMENT -----------------------------------
+# Retweet transaction
+# @client.home_timeline.each do |tweet|
+#     if tweet.user.screen_name != "ichikosai"
+#         p tweet.text
+#         @client.retweet(tweet.id)
+#     end
+# end
+
+# TEST ENVIRONMENT -----------------------------------------
+# Tweet constantly
+@client.update("I'm tweeting with @gem!")
