c49b972a4c65a18d851651b8c69130b60e894d46
Yuzuki Arai <yudukikun5120@gmail.com>
2019-12-22 23:36:31 +0900

    Test

--- retweet.rb
+++ retweet.rb
@@ -9,7 +9,7 @@ require 'twitter'
 end
 
 # PRODUCTION ENVIRONMENT -----------------------------------
-# Retweet transaction
+# Retweet transaction (Home Timeline)
 @client.home_timeline.each do |tweet|
     if tweet.user.screen_name != "ichikosai"
         @client.retweet(tweet.id)
@@ -19,3 +19,20 @@ end
 # TEST ENVIRONMENT -----------------------------------------
 # Tweet constantly
 # @client.update("I'm tweeting with @gem!")
+
+# # Retweet transaction (User Timeline)
+# array = []
+#
+# # Fill in the blank
+# list_name = ""
+# list_members = @client.list_members(list_name)
+#
+# list_members.each do |member|
+#   array.push(member.id)
+# end
+#
+# # Test puts
+# @client.user_timeline(*array).each do |tweet|
+#   p tweet.text
+#   p "-----------------------------------------------------"
+# end
